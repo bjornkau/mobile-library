7ed009855cf8fc21ba09fbb78196dfeb72206378
Bjørn Kaare Aune <bjornkau@stud.ntnu.no>
2018-11-15 17:50:07 +0100

    done with first POST call

--- RESTApi/functions.go
+++ RESTApi/functions.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strconv"
+	//"reflect"
 )
 
 func APIBookURL(isbn int) string {
@@ -15,22 +16,26 @@ func GetBookInfo(w http.ResponseWriter, r *http.Request) {
 	var isbn ISBN
 	err := json.NewDecoder(r.Body).Decode(&isbn)
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	} else {
+		jsonResp, err := RetrieveBookInfo(isbn.Isbn)
+		if err != nil {
+			http.Error(w, http.StatusText(418), 418)
 		} else {
-			jsonResp, err := RetrieveBookInfo(isbn.Isbn)
-			if err != nil {
-				http.Error(w, http.StatusText(418),418)
+			book, check := ParseBookInfo(jsonResp, isbn.Isbn)
+			if !check {
+				http.Error(w, http.StatusText(500), 500)
 			} else {
-				//fmt.Fprint(w,jsonResp)
-				ParseBookInfo(jsonResp,isbn.Isbn,w)
+				json.NewEncoder(w).Encode(book)
 			}
-
 		}
+
+	}
 }
 
-func RetrieveBookInfo(isbn int) (jsonResp map[string]interface{}, err error){
+func RetrieveBookInfo(isbn int) (jsonResp map[string]interface{}, err error) {
 	resp, err := http.Get(APIBookURL(isbn))
-	
+
 	if err != nil {
 		return
 	} else {
@@ -40,30 +45,46 @@ func RetrieveBookInfo(isbn int) (jsonResp map[string]interface{}, err error){
 }
 
 /*type Book struct {
-	Title       string            `json:"title"`
-	Subtitle    string            `json:"subtitle"`
-	Publishers  []string          `json:"publishers"`
-	Pages       int               `json:"pages"`
-	CoverURL    map[string]string `json:"cover"`
-	Authors     map[string]string `json:"authors"`
+	Authors     []string `json:"authors"`
 	Subjects    []string          `json:"subjects"`
-	ISBN        int               `json:"isbn"`
-	PublishDate string            `json:"publish_date"`
 }*/
 
-func ParseBookInfo(jsonResp map[string]interface{}, isbn int, w http.ResponseWriter)(book Book, allIsWell bool){
-	var info map[string]interface{}
+func ParseBookInfo(jsonResp map[string]interface{}, isbn int) (book Book, allIsWell bool) {
 	allIsWell = true
 	bookInfo, ok := jsonResp["ISBN:"+strconv.Itoa(isbn)].(map[string]interface{})
 	if ok {
-		fmt.Fprint(w,info)
+		//fmt.Fprint(w,info)
 		var title bool
+		var subtitle bool
+		var publishDate bool
+		var author bool
+		var subject bool
+		book.ISBN = isbn
 		book.Title, title = bookInfo["title"].(string)
-
-		} else {
-			allIsWell = false
-			return
+		book.Subtitle, subtitle = bookInfo["subtitle"].(string)
+		book.Pages = int(bookInfo["number_of_pages"].(float64))
+		book.PublishDate, publishDate = bookInfo["publish_date"].(string)
+		//
+		authors, author := bookInfo["authors"].([]interface{})
+		for i := 0; i < len(authors); i++ {
+			tempAuthor := authors[i].(map[string]interface{})
+			book.Authors = append(book.Authors, tempAuthor["name"].(string))
 		}
+
+		subjects, subject := bookInfo["subjects"].([]interface{})
+		for i := 0; i < len(subjects); i++ {
+			tempSubject := subjects[i].(map[string]interface{})
+			book.Subjects = append(book.Subjects, tempSubject["name"].(string))
+		}
+
+		if !title || !subtitle || !publishDate || !author || !subject {
+			allIsWell = false
+			fmt.Printf("this should not happen, things have broken")
+		}
+
+	} else {
+		allIsWell = false
+		return
+	}
 	return
 }
-
--- RESTApi/structs.go
+++ RESTApi/structs.go
@@ -1,15 +1,13 @@
 package main
 
 type Book struct {
-	Title       string            `json:"title"`
-	Subtitle    string            `json:"subtitle"`
-	Publishers  []string          `json:"publishers"`
-	Pages       int               `json:"pages"`
-	CoverURL    map[string]string `json:"cover"`
-	Authors     map[string]string `json:"authors"`
-	Subjects    []string          `json:"subjects"`
-	ISBN        int               `json:"isbn"`
-	PublishDate string            `json:"publish_date"`
+	Title       string   `json:"title"`
+	Subtitle    string   `json:"subtitle"`
+	Pages       int      `json:"pages"`
+	Authors     []string `json:"authors"`
+	Subjects    []string `json:"subjects"`
+	ISBN        int      `json:"isbn"`
+	PublishDate string   `json:"publish_date"`
 }
 
 type Author struct {
